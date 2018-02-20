7ba3e39e567a0e7063604067d904cae617af20b9
Ameet Sarkar <sarkar.ameet28@gmail.com>
2018-02-19 20:50:26 -0600

    added lambda to run intent

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const APP_ID = 'amzn1.ask.skill.e0929fb0-ad82-43f5-b785-95eee4ddef38';
 
 const handlers = {
     'CtaIntent': function () {
-        request('http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=541afb8f3df94db2a7afffc486ea4fbf&mapid=40530&rt=Brn', function (error, response, body) {
-            this.emit(':tell', 'Hello peeps');
-            console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
-        });
+            request('http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=541afb8f3df94db2a7afffc486ea4fbf&mapid=40530&rt=Brn',  (error, response, body) => {
+                this.emit(':tell', 'Hello peepzzzzzz');
+                console.log('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', body); // Print the HTML for the Google homepage.
+            });
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
