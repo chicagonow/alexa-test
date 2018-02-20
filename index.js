122d6f70a4f871de0117a9994c153b947e92c05f
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-02-19 18:39:03 -0600

    added initial alexa stuff

---
+++ index.js
@@ -0,0 +1,30 @@
+const Alexa = require('alexa-sdk');
+
+const APP_ID = 'amzn1.ask.skill.e0929fb0-ad82-43f5-b785-95eee4ddef38';
+
+const handlers = {
+    'CtaIntent': function() {
+        this.emit(':tell', 'Hello peeps');
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = this.t('HELP_MESSAGE');
+        const reprompt = this.t('HELP_MESSAGE');
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'Unhandled': function () {
+        this.emit(':tell', "You don goofed");
+    }
+};
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
