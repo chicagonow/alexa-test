02d6fda106a752c1063f048d6df65df99027b3a6
Ameet Sarkar <sarkar.ameet28@gmail.com>
2018-02-19 20:17:51 -0600

    added request to CTA API for Diversey Brown Line. Added test and package json

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const Alexa = require('alexa-sdk');
+const request = require('request');
 
 const APP_ID = 'amzn1.ask.skill.e0929fb0-ad82-43f5-b785-95eee4ddef38';
 
 const handlers = {
-    'CtaIntent': function() {
-        this.emit(':tell', 'Hello peeps');
+    'CtaIntent': function () {
+        request('http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=541afb8f3df94db2a7afffc486ea4fbf&mapid=40530&rt=Brn', function (error, response, body) {
+            this.emit(':tell', 'Hello peeps');
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+        });
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
@@ -28,3 +34,14 @@ exports.handler = function (event, context) {
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
+
+exports.requestTest = function (event, context) {
+    console.log("hello");
+    request('http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=541afb8f3df94db2a7afffc486ea4fbf&mapid=40530&rt=Brn', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+}
+
+require('make-runnable');
