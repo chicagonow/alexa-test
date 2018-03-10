ee934bf7fcd4de4db4d23b1f4ef23d6725890b98
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-03-10 14:41:44 -0600

    added geocoder to LocationHandler

--- lambda/custom/handlers/location/LocationHandler.js
+++ lambda/custom/handlers/location/LocationHandler.js
@@ -31,8 +31,7 @@ exports.getLocation = (apiEndpoint, token, deviceId, callback) => {
 
     // Call the Amazon API to get the device's location
     request(options, (error, response, body) => {
-        let location = response.statusCode === 200 ? JSON.parse(body) : DEFAULT_LOCATION;
-  // callback && callback(location);
+        let location = response.statusCode === 200 ? JSON.parse(body) : DEFAULT_LOCATION;  
         let locationString = location.addressLine1 + ',' + location.city + ',' + location.stateOrRegion;
         geocoder.getLatLong(locationString, callback);
     });
