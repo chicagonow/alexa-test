e1dcdb3fa34c3ae5f90a6d92d81477134840d955
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-03-10 14:19:06 -0600

    Fixed location handler test

--- lambda/test/handlers/location/locationTest.js
+++ lambda/test/handlers/location/locationTest.js
@@ -1,9 +1,10 @@
 const LocationHandler = require('../../../custom/handlers/location/LocationHandler');
 const alexaJson = require('../../example-alexa-json.json');
 
-let latLongTest = () => {}
+let latLongTest = () => {
     LocationHandler.getLocation(alexaJson.context.System.apiEndpoint, 
-        alexaJson.context.System.apiAccessToken, alexaJson.context.System.device.deviceId, (location) => {            
+        alexaJson.context.System.apiAccessToken, 
+        alexaJson.context.System.device.deviceId, (location) => {            
         if (location) {
             console.log(location.latitude);
             console.log(location.longitude);
@@ -11,7 +12,7 @@ let latLongTest = () => {}
             throw "Location was not found";
         }
     });
-;
+};
 
 exports.all = () => {
     latLongTest();
