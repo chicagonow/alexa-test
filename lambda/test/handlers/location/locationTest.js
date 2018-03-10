a91174145eb2f19552505d0be715d033ebafa94b
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-03-10 14:30:22 -0600

    Added make runnable to test file

--- lambda/test/handlers/location/locationTest.js
+++ lambda/test/handlers/location/locationTest.js
@@ -2,14 +2,14 @@ const LocationHandler = require('../../../custom/handlers/location/LocationHandl
 const alexaJson = require('../../example-alexa-json.json');
 
 let latLongTest = () => {
-    LocationHandler.getLocation(alexaJson.context.System.apiEndpoint, 
-        alexaJson.context.System.apiAccessToken, 
-        alexaJson.context.System.device.deviceId, (location) => {            
+    LocationHandler.getLocation(alexaJson.context.System.apiEndpoint, alexaJson.context.System.apiAccessToken, alexaJson.context.System.device.deviceId, (location) => {            
         if (location) {
             console.log(location.latitude);
             console.log(location.longitude);
         } else {
-            throw "Location was not found";
+            let errorMessage = "Location was not found";            
+            console.log(errorMessage);
+            throw errorMessage;
         }
     });
 };
@@ -17,3 +17,5 @@ let latLongTest = () => {
 exports.all = () => {
     latLongTest();
 };
+
+require('make-runnable');
