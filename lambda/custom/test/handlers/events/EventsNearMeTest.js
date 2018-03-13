a4452a84be7d2f22afe64491f8af3dc4d6d5e73e
ajsarkar28 <sarkar.ameet28@gmail.com>
2018-03-12 19:52:39 -0500

    Set lat/long for searchEvents using locationHandler (#26)
    * added funcitonality to use locationHandler to set values for lat/long in EventsHandler. Tested for events near me in EventsNearMeTest
    
    * commented out events console.log call in index.test
    
    * commented out test in index.test. test written before was not taking in correct parameters

---
+++ lambda/custom/test/handlers/events/EventsNearMeTest.js
@@ -0,0 +1,16 @@
+const EventsHandler = require('../../../handlers/events/EventsHandler');
+const alexaJson = require('../../example-alexa-json.json');
+const ParameterHelper = require('../../../helpers/ParameterHelper');
+
+let searchEventsNearMeTest = () => {
+    let parameters = ParameterHelper.getLocationParameters(alexaJson.context.System);
+    EventsHandler.searchEvents(parameters, (alexaResponse) => {
+        console.log(alexaResponse);
+    })
+};
+
+exports.all = () => {
+    searchEventsNearMeTest();
+};
+
+require('make-runnable')
