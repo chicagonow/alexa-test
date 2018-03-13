a270481e7d6f8cde9feff89ad80548c248e84330
Ameet Sarkar <sarkar.ameet28@gmail.com>
2018-03-12 19:18:27 -0500

    added funcitonality to use locationHandler to set values for lat/long in EventsHandler. Tested for events near me in EventsNearMeTest

--- lambda/custom/handlers/events/EventsHandler.js
+++ lambda/custom/handlers/events/EventsHandler.js
@@ -1,27 +1,36 @@
 const request = require('request');
 const buildUrl = require('build-url');
 const EventsResponseBuilder = require('./EventsResponseBuilder');
+const LocationHandler = require('../location/LocationHandler')
 
 //used sample token,replace later. 
 const AUTH_TOKEN = 'IO6EB7MM6TSCIL2TIOHC';
 const EVENTBRITE_API_DOMAIN = 'https://www.eventbriteapi.com';
 const EVENTBRITE_API_PATH = '/v3/events/search/';
 
-exports.searchEvents = (callback) => {
+exports.searchEvents = (parameters, callback) => {
+
+	LocationHandler.getLocation(parameters.apiEndpoint, parameters.token, parameters.deviceID, (location) => {
+		searchEventbrite(location.latitude, location.longitude, callback);
+	});
+}
+
+let searchEventbrite = (latitude, longitude, callback) => {
+
 	const qp = {};
 	qp[encodeURIComponent('token')] = AUTH_TOKEN;
 	qp[encodeURIComponent('location.within')] = '1mi';
-	qp[encodeURIComponent('location.latitude')] = '41.878440';
-	qp[encodeURIComponent('location.longitude')] = '-87.625622';
+	qp[encodeURIComponent('location.latitude')] = latitude;
+	qp[encodeURIComponent('location.longitude')] = longitude;
 
 	let url = buildUrl(EVENTBRITE_API_DOMAIN, {
 		path: EVENTBRITE_API_PATH,
 		queryParams: qp
 	});
 
-	request(url, (error, response, body) => {		
-        let alexaResponse = EventsResponseBuilder.buildAlexaResponse(JSON.parse(body));
-		callback && callback(alexaResponse, error, response);		
+	request(url, (error, response, body) => {
+		let alexaResponse = EventsResponseBuilder.buildAlexaResponse(JSON.parse(body));
+		callback && callback(alexaResponse, error, response);
 	});
 };
 
