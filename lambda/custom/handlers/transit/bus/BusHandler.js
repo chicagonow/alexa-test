1b3b2062d7627835b9c1064662d851018cf15a7b
baumannalexj <baumannalexj@gmail.com>
2018-03-12 20:33:23 -0500

    renmoved console logs

--- lambda/custom/handlers/transit/bus/BusHandler.js
+++ lambda/custom/handlers/transit/bus/BusHandler.js
@@ -11,7 +11,6 @@ const CTABUS_API_DOMAIN = 'http://ctabustracker.com';
 const CTABUS_API_PATH = '/bustime/api/v2/getpredictions';
 
 exports.getBusesForRouteAndStop = (parameters, callback) => {
-    console.log("Entered BusHandler.getBusesForRouteAndStop");
     let url = buildUrl(CTABUS_API_DOMAIN, {
         path: CTABUS_API_PATH,
         queryParams: {
@@ -22,11 +21,8 @@ exports.getBusesForRouteAndStop = (parameters, callback) => {
         }
         
     });
-    console.log(url);
 
     request(url,  (error, response, body) => {      
-        console.log("entered request method");
-        console.log(body);  
         let alexaResponse = BusResponseBuilder.buildAlexaResponse(JSON.parse(body));
         callback(alexaResponse);        
     });
@@ -36,11 +32,7 @@ exports.getBusesForRouteAndStop = (parameters, callback) => {
 Call getLocation, forward longitude and parameters entered to Bus Stop Repository
 */
 exports.searchBusNearMe = (parameters, callback) => {
-    console.log("BusHandler.searchBusNearMe() routing to LocationHandler");
     LocationHandler.getLocation(parameters.apiEndpoint, parameters.token, parameters.deviceID, (location) => {
-        console.log("latitude: " + location.latitude);
-        console.log("longitude: " + location.longitude);
-        console.log("Parameters input into getNearestBusStopId \n" + util.inspect(parameters, false, null));
         BusRepository.getNearestBusStopId(parameters, location.latitude, location.longitude, (stopID) => {
             parameters.stpid = stopID;
             this.getBusesForRouteAndStop(parameters, callback);
