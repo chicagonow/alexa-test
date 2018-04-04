86eb564bc11747865d19ce1632d08ac902bf3d33
baumannalexj <baumannalexj@gmail.com>
2018-04-04 14:34:33 -0500

    trying with backup

--- resolveSkillAndLambdaVariables.sh
+++ resolveSkillAndLambdaVariables.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env sh
 # replace {BRANCH} with current branch, or use stage or prod .ask and skill.json configs
 
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
@@ -13,8 +13,8 @@ then
     mv .ask/config-prod .ask/config
     mv skill-prod.json skill.json
 else
-    sed -i '' 's/{BRANCH}/'${BRANCH}'/' ./.ask/config
-    sed -i '' 's/{BRANCH}/'${BRANCH}'/' ./skill.json
+    sed -i.bak s/{BRANCH}/${BRANCH}/ .ask/config
+    sed -i.bak s/{BRANCH}/${BRANCH}/ skill.json
 fi
 
 
