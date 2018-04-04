01402c9d4282820b16f2acceb51533d3762d479d
baumannalexj <baumannalexj@gmail.com>
2018-04-04 14:52:32 -0500

    Revert "updated shebang to bash"
    This reverts commit 8858b7ecdb52c9d798ebf2df6b5e2bde63cd04e2.

--- resolveSkillAndLambdaVariables.sh
+++ resolveSkillAndLambdaVariables.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # replace {BRANCH} with current branch, or use stage or prod .ask and skill.json configs
 
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
