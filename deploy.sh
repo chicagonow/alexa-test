a4d4644a9869c89b0f381ea7978cd07d141a6ca9
baumannalexj <baumannalexj@gmail.com>
2018-04-07 18:49:54 -0500

    create region-west profile in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ mkdir ~/.aws
 mkdir ~/.ask
 
 # Create AWS credentials
-echo "[default]" > ~/.aws/credentials
+echo "[region-west]" > ~/.aws/credentials
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 
