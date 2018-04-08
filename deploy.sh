8e1e28893b22bb5890f0cf3eed4d43005e1f8141
baumannalexj <baumannalexj@gmail.com>
2018-04-07 19:02:50 -0500

    lessen diff with stage

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 
 # Create ASK config
-sed -e s/ASK_ACCESS_TOKEN_WEST/${ASK_ACCESS_TOKEN_WEST}/g -e \
-    s/ASK_REFRESH_TOKEN_WEST/${ASK_REFRESH_TOKEN_WEST}/g conf/ask_cli.json > ~/.ask/cli_config
+sed -e s/ASK_ACCESS_TOKEN/${ASK_ACCESS_TOKEN_WEST}/g -e \
+    s/ASK_REFRESH_TOKEN/${ASK_REFRESH_TOKEN_WEST}/g conf/ask_cli.json > ~/.ask/cli_config
 
