1cead5934cac02054e6c00ef5bbf7082ae65f789
baumannalexj <baumannalexj@gmail.com>
2018-04-08 15:03:06 -0500

    fixed event response with special characters

--- lambda/custom/handlers/events/EventsResponseBuilder.js
+++ lambda/custom/handlers/events/EventsResponseBuilder.js
@@ -1,23 +1,25 @@
 const NUMBER_OF_EVENTS = 3;
 
 exports.buildAlexaResponse = (jsonObject) => {
-    let msg = "Here are " + NUMBER_OF_EVENTS + " events going on in Chicago ";
-
-    msg += "<break time=\"1s\"/>" +
-        "<speak>";
+    let msg = "Here are " + NUMBER_OF_EVENTS + " events going on in Chicago. ";
 
     let eventsArray = buildEventArray(jsonObject.events);
-
     msg += eventsArray.join(", ");
-    msg += "</speak>";
 
+    console.info("built event response: " + msg);
     return msg;
 };
 
 let buildEventArray = (events) => {
     let eventsArray = [];
     for (let index = 0; index < NUMBER_OF_EVENTS; index++) {
-        eventsArray.push("<s>" + events[index].name.text + "</s>");
+        eventsArray.push(
+            events[index].name.text
+                .toLocaleLowerCase()
+                .replace("[^a-z\d\s:]", " ")
+                .replace("®", "")
+                .replace("&", " and ")
+        );
     }
     return eventsArray;
 };
