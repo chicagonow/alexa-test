ac5bfec1e9d923c6ebc497979bd2d011b7377e1a
Ameet Sarkar <sarkar.ameet28@gmail.com>
2018-04-14 16:09:14 -0500

    deleted callback function in events handler

--- lambda/custom/handlers/events/EventsHandler.js
+++ lambda/custom/handlers/events/EventsHandler.js
@@ -9,32 +9,6 @@ const AUTH_TOKEN = 'IO6EB7MM6TSCIL2TIOHC';
 const EVENTBRITE_API_DOMAIN = 'https://www.eventbriteapi.com';
 const EVENTBRITE_API_PATH = '/v3/events/search/';
 
-exports.searchEventsNearMe = (parameters, callback) => {
-
-	LocationHandler.getLocation(parameters.apiEndpoint, parameters.token, parameters.deviceID, (location) => {
-		searchEventbrite(location.latitude, location.longitude, callback);
-	});
-};
-
-let searchEventbrite = (latitude, longitude, callback) => {
-
-	const qp = {};
-	qp[encodeURIComponent('token')] = AUTH_TOKEN;
-	qp[encodeURIComponent('location.within')] = '1mi';
-	qp[encodeURIComponent('location.latitude')] = latitude;
-	qp[encodeURIComponent('location.longitude')] = longitude;
-
-	let url = buildUrl(EVENTBRITE_API_DOMAIN, {
-		path: EVENTBRITE_API_PATH,
-		queryParams: qp
-	});
-
-	request(url, (error, response, body) => {
-		let alexaResponse = EventsResponseBuilder.buildAlexaResponse(JSON.parse(body));
-		callback && callback(alexaResponse, error, response);
-	});
-};
-
 //return Alexa response string
 exports.asyncGetEventsNearLocation = async function asyncGetEventsNearLocation(latitude, longitude){
 	const qp = {};
@@ -61,5 +35,3 @@ exports.asyncGetEventsNearLocation = async function asyncGetEventsNearLocation(l
     }
 	return alexaEventResponse;
 };
-
-require('make-runnable');
