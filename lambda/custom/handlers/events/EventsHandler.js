e02323c17b2aadeb7379c3e414d52c255e471846
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-04-15 15:01:59 -0500

    Added asyncGetEventsWithinTimeFrame function along with test

--- lambda/custom/handlers/events/EventsHandler.js
+++ lambda/custom/handlers/events/EventsHandler.js
@@ -17,9 +17,7 @@ exports.searchEventsNearMe = (parameters, callback) => {
 };
 
 let searchEventbrite = (latitude, longitude, callback) => {
-
-	const qp = {};
-	qp[encodeURIComponent('token')] = AUTH_TOKEN;
+	let qp = getCommonQueryObjectParameters();
 	qp[encodeURIComponent('location.within')] = '1mi';
 	qp[encodeURIComponent('location.latitude')] = latitude;
 	qp[encodeURIComponent('location.longitude')] = longitude;
@@ -37,8 +35,7 @@ let searchEventbrite = (latitude, longitude, callback) => {
 
 //return Alexa response string
 exports.asyncGetEventsNearUserLocation = async function asyncGetEventsNearUserLocation(latitude, longitude){
-	const qp = {};
-	qp[encodeURIComponent('token')] = AUTH_TOKEN;
+	let qp = getCommonQueryObjectParameters();
 	qp[encodeURIComponent('location.within')] = '1mi';
 	qp[encodeURIComponent('location.latitude')] = latitude;
 	qp[encodeURIComponent('location.longitude')] = longitude;
@@ -60,6 +57,45 @@ exports.asyncGetEventsNearUserLocation = async function asyncGetEventsNearUserLo
         alexaEventResponse = "There was an error with the event service. Try again soon."
     }
 	return alexaEventResponse;
+};
+
+/**
+ * Returns an alexa response with events occurring within the given time frame
+ * @param {Date} startDate 
+ * @param {Date} endDate 
+ */
+exports.asyncGetEventsWithinTimeFrame = async function asyncGetEventsWithinTimeFrame(latitude, longitude, startDate, endDate) {
+	let qp = getCommonQueryObjectParameters();
+	qp[encodeURIComponent('location.within')] = '1mi';
+	qp[encodeURIComponent('location.latitude')] = latitude;
+	qp[encodeURIComponent('location.longitude')] = longitude;
+	qp[encodeURIComponent('start_date.range_start')] = startDate.toISOString().split('.')[0]+"Z" ;
+	qp[encodeURIComponent('start_date.range_end')] = endDate.toISOString().split('.')[0]+"Z" ;
+
+	let url = buildUrl(EVENTBRITE_API_DOMAIN, {
+		path: EVENTBRITE_API_PATH,
+		queryParams: qp
+	});
+
+    let body = await asyncRequest(url)
+        .catch(err => console.error(err));
+
+	let alexaEventResponse = "";	
+	try {
+        alexaEventResponse  = EventsResponseBuilder.buildAlexaResponse(JSON.parse(body));
+    } catch (err) {
+        console.error("event response body was: " + body);
+        console.error(err);
+        alexaEventResponse = "There was an error with the event service. Try again soon."
+    }
+	return alexaEventResponse;
+}
+
+// Returns common query parameters
+let getCommonQueryObjectParameters = () => {
+	let qp = {};
+	qp[encodeURIComponent('token')] = AUTH_TOKEN;
+	return qp;
 };
 
 require('make-runnable');
--- lambda/custom/test/handlers/events/EventsHandlerTest.js
+++ lambda/custom/test/handlers/events/EventsHandlerTest.js
@@ -6,6 +6,9 @@ const alexaJson = require('../../response.alexa.json');
 const responseDeviceLocation = require('../../response.deviceLocation');
 const responseEvents = require('../../response.events');
 
+// Time response
+const responseToday = require('../../response.eventsTime');
+
 
 const EventsHandler = require('../../../handlers/events/EventsHandler');
 const ParameterHelper = require('../../../helpers/ParameterHelper');
@@ -16,24 +19,26 @@ describe('EventsHandler Tests', function() {
     let sandbox;
 
     beforeEach(function() {
-
         let deviceId = alexaJson.context.System.device.deviceId;
         nock('https://api.amazonalexa.com')
             .get('/v1/devices/' + deviceId + '/settings/address')
             .query(true)
             .reply(200, responseDeviceLocation);
 
-        sandbox = sinon.sandbox.create();        
+        sandbox = sinon.sandbox.create();       
 
         nock('https://www.eventbriteapi.com')
             .get('/v3/events/search/')
-            .query(true)
-            .reply(200, responseEvents);
+            .query(function(queryObject) {
+                return !queryObject.start_date;
+            })
+            .reply(200, responseEvents);        
         
     });
 
     afterEach(function() {
         sandbox.restore();
+        nock.cleanAll();
     });
 
     // Tests the searchEventsNearMe method
@@ -54,6 +59,31 @@ describe('EventsHandler Tests', function() {
         it('async-await call returns correct Alexa Response', async function(){
             let alexaResponse = await EventsHandler.asyncGetEventsNearUserLocation(-10, -81.7);
             assert.equal(alexaResponse, expectedEventsResponse);
+        });
+    });
+
+    // Tests the searchEventsWithinTimeFrame method
+    describe("searchEventsWithinTimeFrame", () => {
+
+        beforeEach(function() {
+            nock.cleanAll();
+            
+            // Mock response for events between a certain time range
+            nock('https://www.eventbriteapi.com')
+            .get('/v3/events/search/')
+            .query(function(queryObject) {
+                return queryObject["start_date.range_start"] === "2018-05-15T05:00:00Z"
+                    && queryObject["start_date.range_end"] === "2018-05-16T04:00:00Z";
+            })
+            .reply(200, responseToday);
+        });
+
+        it('returns events for today', async function() {
+            let expectedResponse = "Here are 3 events going on in Chicago. chicago professional  and  technology diversity career fair, 10th stem cell clonality and genome stability retreat, made to win rooftop social ";
+            let startDate = new Date("2018-05-15T00:00:00"); // replace with start date
+            let endDate = new Date("2018-05-15T23:00:00"); // replace with end date
+            let alexaResponse = await EventsHandler.asyncGetEventsWithinTimeFrame(41.87893, -87.626088, startDate, endDate);
+            assert.equal(alexaResponse, expectedResponse);
         });
     });
 
