08c59300404b434f5e865c034970ed23bbeac2ad
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-04-17 19:52:50 -0500

    Fixed response

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -46,8 +46,12 @@ const handlers = {
     'EventTimeFrameIntent': async function() {
         let timeFrame = this.event.request.intent.slots.timeFrame.value;
         let parameters = ParameterHelper.getLocationParameters(this.event.context.System);
-        let alexaResponse = await IntentController.getEventsWithinTimeFrame(parameters.apiEndpoint, parameters.token, parameters.deviceID, timeFrame);
-        this.emit('tell', alexaResposne);
+        let alexaResponse = 
+            await IntentController.getEventsWithinTimeFrame(parameters.apiEndpoint, parameters.token, parameters.deviceID, timeFrame)
+                .catch(error => {
+                    console.error(error)
+                });
+        this.emit('tell', alexaResponse);
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
