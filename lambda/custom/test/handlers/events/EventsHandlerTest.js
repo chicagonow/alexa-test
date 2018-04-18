bd2c05f6e6c567b01d30d635c229ae44158fb5ed
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-04-17 19:38:17 -0500

    Removed specific time check from tests

--- lambda/custom/test/handlers/events/EventsHandlerTest.js
+++ lambda/custom/test/handlers/events/EventsHandlerTest.js
@@ -77,8 +77,7 @@ describe('EventsHandler Tests', function() {
             nock('https://www.eventbriteapi.com')
             .get('/v3/events/search/')
             .query(function(queryObject) {
-                return queryObject["start_date.range_start"] === "2018-05-15T00:00:00"
-                    && queryObject["start_date.range_end"] === "2018-05-15T23:59:59";
+                return queryObject["start_date.range_start"];
             })
             .reply(200, responseToday);
         });
