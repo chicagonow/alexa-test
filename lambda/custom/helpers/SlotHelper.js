e651012838821d85b12b8722340d4677c6f3c68f
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-05-22 00:48:54 -0500

    updated to TransitSlotHelper and all tests pass

--- lambda/custom/helpers/SlotHelper.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * Returns an array of objects {slotName, value, failType} with any invalid slots
- * @param {object} slotObject 
- * @param {array} slotNamesToCheck 
- */
-exports.checkSlots = (slotObject, slotNamesToCheck) => {
-    
-};
