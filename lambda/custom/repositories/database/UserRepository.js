880154aea5e746d0c1a270c01a4ad560f4e12e8d
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-06-04 21:01:00 -0500

    Added logging to repository

--- lambda/custom/repositories/database/UserRepository.js
+++ lambda/custom/repositories/database/UserRepository.js
@@ -1,10 +1,11 @@
 const asyncRequest = require('request-promise');
+const logger = require("../../logging/Logger");
 
 /**
  * Adds or updates a user in the databae
  * @param {string} userID 
  */
-exports.updateUser = function(userID) {
+exports.updateUser = async function(userID) {
     var options = {
         method: 'POST',
         uri: process.env.USER_UPDATE_URL,
@@ -13,7 +14,9 @@ exports.updateUser = function(userID) {
         }
     };
 
-    asyncRequest(options);        
+    let response = await asyncRequest(options).catch(error => {
+        logger.error("error with user data upload: " + error.message);
+    });
 };
 
 
