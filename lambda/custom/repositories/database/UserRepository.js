35069fc2dc104526b99de8e33a4cd958856e5891
rknruben56 <rodrigrd.rosehulman@gmail.com>
2018-06-04 20:33:21 -0500

    Updated UserRepo to use HTTP url

--- lambda/custom/repositories/database/UserRepository.js
+++ lambda/custom/repositories/database/UserRepository.js
@@ -1,39 +1,19 @@
-const aws = require('aws-sdk');
-const logger = require("../../logging/Logger");
+const asyncRequest = require('request-promise');
 
 /**
  * Adds or updates a user in the databae
  * @param {string} userID 
  */
 exports.updateUser = async function(userID) {
-    let lambda = getLambda();
-
-    // Set the payload to send to the database service
-    let payload = {
-        httpMethod: "PUT",
-        parameters: {
-            TableName: "User",
-            UserID: userID
+    var options = {
+        method: 'POST',
+        uri: process.env.USER_UPDATE_URL,
+        body: {
+            userID: userID
         }
     };
 
-    // Invoke the database service and pass the payload
-    lambda.invoke({
-        FunctionName: 'DatabaseService',
-        Payload: JSON.stringify(payload)
-    }, (error, data) => {
-        if (error) {
-            logger.error("Error adding user: " + error.message);
-        } else {
-            logger.info("Added to User table user: " + userID);
-        }
-    });
-};
-
-let getLambda = () => {
-    return new aws.Lambda({
-        region: 'us-east-1'
-    });
+    asyncRequest(options);        
 };
 
 
